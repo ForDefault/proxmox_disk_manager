8852a9a5a501386a4181bf64c2b34abe83d5dac3
ForDefault <106802923+ForDefault@users.noreply.github.com>
2024-10-09 02:08:55 -0500

    Update gather_information.sh

--- gather_information.sh
+++ gather_information.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 # Step 1: Get the list of disks (excluding LVM, loop, and unwanted entries)
 get_disks() {
     lsblk -dn -o NAME | grep -vE "loop|lvm--|local--lvm" > disk_list.txt
@@ -19,61 +19,119 @@ map_disks_to_partitions() {
 
     while IFS= read -r disk; do
         echo "$disk:" >> mapped_disks_and_partitions.txt
-        # Find partitions that belong to this disk, ensuring not to list the disk itself
-        grep "^$disk" partition_list.txt | grep -v "^$disk$" | while read -r partition; do
-            echo "  - $partition" >> mapped_disks_and_partitions.txt
-        done
+
+        # Capture the partitions and check if any exist directly under this disk
+        partitions=$(grep "^$disk" partition_list.txt | grep -v "^$disk$")
+
+        if [ -n "$partitions" ]; then
+            # If partitions are found, write them under the disk
+            echo "$partitions" | while read -r partition; do
+                echo "  - $partition" >> mapped_disks_and_partitions.txt
+            done
+        else
+            # No partitions found, add the disk itself as a partition
+            echo "  - $disk" >> mapped_disks_and_partitions.txt
+        fi
     done < disk_list.txt
 }
 
-# Function to extract information using blkid, lsblk, and udevadm
-extract_info() {
+# Function to extract information for disks
+extract_info_disk() {
     local device=$1
     local key=$2
 
     case $key in
+        # 1
         "uuid")
-            blkid -s UUID -o value "/dev/$device" | head -n 1
+            blkid -s PTUUID -o value "/dev/$device" 2>/dev/null || echo ""
             ;;
-        "partuuid")
-            blkid -s PARTUUID -o value "/dev/$device" | head -n 1
-            ;;
+        # 2
         "fs_type")
-            blkid -s TYPE -o value "/dev/$device" | head -n 1
+            echo ""  # No fs_type for entire disks
             ;;
+        # 3
         "mount_path")
-            mount | grep "/dev/$device" | awk '{print $3}' | head -n 1mount | grep "/dev/$device" | awk '{print $3}' | head -n 1
+            echo ""  # Disks generally do not have mount paths
             ;;
-        "size")
+        # 4
+        "size" | "total_size")
             lsblk -no SIZE "/dev/$device" | head -n 1
             ;;
+        # 5
         "id_serial")
             udevadm info --query=property --name="/dev/$device" | grep "ID_SERIAL_SHORT=" | cut -d '=' -f2 | head -n 1
             ;;
+        # 6
         "id_model")
             udevadm info --query=property --name="/dev/$device" | grep "ID_MODEL=" | cut -d '=' -f2 | head -n 1
             ;;
+        # 7
         "pcie_path")
             udevadm info --query=path --name="/dev/$device" | head -n 1
             ;;
+        # 8
         "devpath")
-            # Modified command to match the device dynamically
-            readlink -f /dev/disk/by-path/* 2>/dev/null | grep -Fx "/dev/$device" | head -n 1
+            echo "/dev/$device"
             ;;
+        # 9
         "diskseq")
             udevadm info --query=property --name="/dev/$device" | grep "DISKSEQ=" | cut -d '=' -f2 | head -n 1
             ;;
-        "used")
-            df -h | grep "^/dev/$device" | awk '{print $3}' | head -n 1
+        # 10
+        "used" | "percentage_used")
+            df -h "/dev/$device" 2>/dev/null | awk 'NR==2 {print $3}'
             ;;
-        "percentage_used")
-            df -h | grep "^/dev/$device" | awk '{print $5}' | head -n 1
-            ;;
-        "total_size")
-            lsblk -dn -o SIZE "/dev/$device" | head -n 1
-            ;;
+        # 11
         "is_usb")
             udevadm info --query=property --name="/dev/$device" | grep -q "ID_BUS=usb" && echo true || echo false
+            ;;
+        *)
+            echo "Unknown key: $key"
+            ;;
+    esac
+}
+
+# Function to extract information for partitions
+extract_info_partition() {
+    local device=$1
+    local key=$2
+
+    case $key in
+        # 1
+        "uuid")
+            blkid -s UUID -o value "/dev/$device" | head -n 1
+            ;;
+        # 2
+        "fs_type")
+            blkid -s TYPE -o value "/dev/$device" | head -n 1
+            ;;
+        # 3
+        "mount_path")
+            mount | grep "/dev/$device" | awk '{print $3}' | head -n 1
+            ;;
+        # 4
+        "size")
+            lsblk -no SIZE "/dev/$device" | head -n 1
+            ;;
+        # 5
+        "id_serial")
+            udevadm info --query=property --name="/dev/$device" | grep "ID_SERIAL_SHORT=" | cut -d '=' -f2 | head -n 1
+            ;;
+        # 6
+        "id_model")
+            udevadm info --query=property --name="/dev/$device" | grep "ID_MODEL=" | cut -d '=' -f2 | head -n 1
+            ;;
+        # 7
+        "pcie_path")
+            udevadm info --query=path --name="/dev/$device" | head -n 1
+            ;;
+        # 8
+        "devpath")
+            echo "/dev/$device"
+            ;;
+        # 9
+        "diskseq")
+            udevadm info --query=property --name="/dev/$device" | grep "DISKSEQ=" | cut -d '=' -f2 | head -n 1
             ;;
         *)
             echo "Unknown key: $key"
@@ -92,38 +150,76 @@ generate_variables() {
 
     # Write the variables to the script file
     {
-        echo "disk_number_${disk_counter}=\"${disk_counter}_${disk_name}\""
-        echo "disk_name_${disk_counter}=\"$disk_name\""
-        echo "disk_size_${disk_counter}=\"$(extract_info "$disk_name" "size")\""
-        echo "disk_type_${disk_counter}=\"$(extract_info "$disk_name" "fs_type")\""
-        echo "is_usb_${disk_counter}=\"$(extract_info "$disk_name" "is_usb")\""
-        echo "total_size_${disk_counter}=\"$(extract_info "$disk_name" "total_size")\""
-        echo "used_${disk_counter}=\"$(extract_info "$disk_name" "used")\""
-        echo "percentage_used_${disk_counter}=\"$(extract_info "$disk_name" "percentage_used")\""
-        echo "uuid_${disk_counter}=\"$(extract_info "$disk_name" "uuid")\""
-        echo "part_size_${disk_counter}=\"$(extract_info "$disk_name" "size")\""
-        echo "fs_type_${disk_counter}=\"$(extract_info "$disk_name" "fs_type")\""
-        echo "mount_path_${disk_counter}=\"$(extract_info "$disk_name" "mount_path")\""
-        echo "mount_status_${disk_counter}=\"mounted\""  # Can be dynamically checked with `mount | grep`
-        echo "id_serial_${disk_counter}=\"$(extract_info "$disk_name" "id_serial")\""
-        echo "id_model_${disk_counter}=\"$(extract_info "$disk_name" "id_model")\""
-        echo "pcie_path_${disk_counter}=\"$(extract_info "$disk_name" "pcie_path")\""
-        echo "devpath_${disk_counter}=\"$(extract_info "$disk_name" "devpath")\""
-        echo "diskseq_${disk_counter}=\"$(extract_info "$disk_name" "diskseq")\""
-        
+        # Disk-level information
+        # 1
+        echo "disk_number_${disk_counter}=${disk_counter}_${disk_name}"
+        # 2
+        echo "disk_name_${disk_counter}=$disk_name"
+        # 3
+        echo "disk_size_${disk_counter}=$(extract_info_disk "$disk_name" "size")"
+        # 4
+        echo "disk_type_${disk_counter}=$(extract_info_disk "$disk_name" "fs_type")"
+        # 5
+        echo "is_usb_${disk_counter}=$(extract_info_disk "$disk_name" "is_usb")"
+        # 6
+        echo "total_size_${disk_counter}=$(extract_info_disk "$disk_name" "total_size")"
+        # 7
+        echo "used_${disk_counter}=$(extract_info_disk "$disk_name" "used")"
+        # 8
+        echo "percentage_used_${disk_counter}=$(extract_info_disk "$disk_name" "percentage_used")"
+        # 9
+        echo "uuid_${disk_counter}=$(extract_info_disk "$disk_name" "uuid")"
+        # 10
+        echo "mount_path_${disk_counter}=""
+        # 11
+        echo "mount_status_${disk_counter}=unmounted"
+        # 12
+        echo "id_serial_${disk_counter}=$(extract_info_disk "$disk_name" "id_serial")"
+        # 13
+        echo "id_model_${disk_counter}=$(extract_info_disk "$disk_name" "id_model")"
+        # 14
+        echo "pcie_path_${disk_counter}=$(extract_info_disk "$disk_name" "pcie_path")"
+        # 15
+        echo "devpath_${disk_counter}=/dev/$disk_name"
+        # 16
+        echo "diskseq_${disk_counter}=$(extract_info_disk "$disk_name" "diskseq")"
+
+        # Partition-level information
         partition_counter=1
         for partition in "${partitions[@]}"; do
-            echo "partition_name_${disk_counter}_${partition_counter}=\"$partition\""
-            echo "uuid_${disk_counter}_${partition_counter}=\"$(extract_info "$partition" "uuid")\""
-            echo "part_size_${disk_counter}_${partition_counter}=\"$(extract_info "$partition" "size")\""
-            echo "fs_type_${disk_counter}_${partition_counter}=\"$(extract_info "$partition" "fs_type")\""
-            echo "mount_path_${disk_counter}_${partition_counter}=\"$(extract_info "$partition" "mount_path")\""
-            echo "mount_status_${disk_counter}_${partition_counter}=\"mounted\""  # Can be dynamically checked with `mount | grep`
-            echo "id_serial_${disk_counter}_${partition_counter}=\"$(extract_info "$partition" "id_serial")\""
-            echo "id_model_${disk_counter}_${partition_counter}=\"$(extract_info "$partition" "id_model")\""
-            echo "pcie_path_${disk_counter}_${partition_counter}=\"$(extract_info "$partition" "pcie_path")\""
-            echo "devpath_${disk_counter}_${partition_counter}=\"$(extract_info "$partition" "devpath")\""
-            echo "diskseq_${disk_counter}_${partition_counter}=\"$(extract_info "$partition" "diskseq")\""
+            # 17
+            echo "partition_name_${disk_counter}_${partition_counter}=$partition"
+            # 18
+            echo "uuid_${disk_counter}_${partition_counter}=$(extract_info_partition "$partition" "uuid")"
+            # 19
+            echo "part_size_${disk_counter}_${partition_counter}=$(extract_info_partition "$partition" "size")"
+            # 20
+            echo "fs_type_${disk_counter}_${partition_counter}=$(extract_info_partition "$partition" "fs_type")"
+            
+            mount_path="$(extract_info_partition "$partition" "mount_path")"
+            if [ -n "$mount_path" ]; then
+                # 21
+                echo "mount_path_${disk_counter}_${partition_counter}=$mount_path"
+                # 22
+                echo "mount_status_${disk_counter}_${partition_counter}=mounted"
+            else
+                # 23
+                echo "mount_path_${disk_counter}_${partition_counter}=""
+                # 24
+                echo "mount_status_${disk_counter}_${partition_counter}=unmounted"
+            fi
+
+            # 25
+            echo "id_serial_${disk_counter}_${partition_counter}=$(extract_info_partition "$partition" "id_serial")"
+            # 26
+            echo "id_model_${disk_counter}_${partition_counter}=$(extract_info_partition "$partition" "id_model")"
+            # 27
+            echo "pcie_path_${disk_counter}_${partition_counter}=$(extract_info_partition "$partition" "pcie_path")"
+            # 28
+            echo "devpath_${disk_counter}_${partition_counter}=/dev/$partition"
+            # 29
+            echo "diskseq_${disk_counter}_${partition_counter}=$(extract_info_partition "$partition" "diskseq")"
+
             partition_counter=$((partition_counter + 1))
         done
     } > "$file_name"
@@ -133,7 +229,18 @@ generate_variables() {
     disk_counter=$((disk_counter + 1))
 }
 
-# Function to generate script files for all disks
+# Main execution
+get_disks
+get_partitions
+map_disks_to_partitions
+
+# Identify disks with no partitions and mark them
+disks_without_partitions=$(awk -F: '$2 ~ /^ *- *$/ {print $1}' mapped_disks_and_partitions.txt)
+for disk in $disks_without_partitions; do
+    echo "$disk" >> disks_without_partitions.txt
+
+done
+
 generate_from_mapping() {
     disk_counter=1
     while IFS= read -r disk; do
@@ -142,10 +249,7 @@ generate_from_mapping() {
     done < disk_list.txt
 }
 
-# Main execution
-get_disks
-get_partitions
-map_disks_to_partitions
 generate_from_mapping
 
 echo "Command substitutes have been generated and saved in the 'disk_command_substitutes' directory."
+cd disk_command_substitutes
